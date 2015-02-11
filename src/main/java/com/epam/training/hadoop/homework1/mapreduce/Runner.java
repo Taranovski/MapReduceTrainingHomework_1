0303fe73c101596040c2cb4da6c554a98ad3c177
Oleksandr_Taranovsky <Oleksandr_Taranovsky@EPUAKYIW0052.kyiv.epam.com>
2015-02-11 18:08:28 +0200

    v0008

--- src/main/java/com/epam/training/hadoop/homework1/mapreduce/Runner.java
+++ src/main/java/com/epam/training/hadoop/homework1/mapreduce/Runner.java
@@ -5,13 +5,49 @@
  */
 package com.epam.training.hadoop.homework1.mapreduce;
 
+import com.epam.training.hadoop.homework1.mapreduce.writable.MyFinalWritable;
+import java.io.IOException;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.SequenceFile.CompressionType;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+
 /**
  *
  * @author Oleksandr_Taranovsky
  */
 public class Runner {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
 
+        JobConf conf = new JobConf();
+
+        conf.set("mapred.output.compression.codec", "org.apache.hadoop.io.compress.SnappyCodec");
+        
+        Job job = Job.getInstance(conf, "Log analyse");
+
+        job.setJarByClass(Runner.class);
+
+        job.setMapperClass(MyMapper.class);
+        job.setCombinerClass(MyCombiner.class);
+        job.setReducerClass(MyReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(MyFinalWritable.class);
+        job.setNumReduceTasks(2);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(SequenceFileOutputFormat.class);
+        
+        TextInputFormat.addInputPath(job, new Path(""));
+        SequenceFileOutputFormat.setOutputPath(job, new Path(""));
+        
+        SequenceFileOutputFormat.setOutputCompressionType(job, CompressionType.BLOCK);
+        SequenceFileOutputFormat.setCompressOutput(job, true);
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
--- src/test/java/com/epam/training/hadoop/homework1/mapreduce/MapReduceTest.java
+++ src/test/java/com/epam/training/hadoop/homework1/mapreduce/MapReduceTest.java
@@ -7,6 +7,7 @@ package com.epam.training.hadoop.homework1.mapreduce;
 
 import com.epam.training.hadoop.homework1.mapreduce.counter.Browsers;
 import com.epam.training.hadoop.homework1.mapreduce.counter.Errors;
+import com.epam.training.hadoop.homework1.mapreduce.writable.MyFinalWritable;
 import com.epam.training.hadoop.homework1.mapreduce.writable.MyIntermediateWritable;
 import java.io.IOException;
 import java.util.LinkedList;
@@ -14,7 +15,8 @@ import java.util.List;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
 import org.apache.hadoop.mrunit.types.Pair;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -27,13 +29,15 @@ import static org.junit.Assert.*;
  *
  * @author Oleksandr_Taranovsky
  */
-public class MyMapperTest {
+public class MapReduceTest {
 
     private static final Mapper MAPPER = new MyMapper();
-//    private Reducer reducer = new WordCountReducer();
-    private static MapDriver<LongWritable, Text, Text, MyIntermediateWritable> mapDriver;
+    private static final MyCombiner COMBINER = new MyCombiner();
+    private static final Reducer REDUCER = new MyReducer();
 
-    public MyMapperTest() {
+    private static MapReduceDriver<LongWritable, Text, Text, MyIntermediateWritable, Text, MyFinalWritable> mapReduceDriver;
+
+    public MapReduceTest() {
     }
 
     @BeforeClass
@@ -46,7 +50,7 @@ public class MyMapperTest {
 
     @Before
     public void setUp() {
-        mapDriver = MapDriver.newMapDriver(MAPPER);
+        mapReduceDriver = MapReduceDriver.newMapReduceDriver(MAPPER, REDUCER, COMBINER);
     }
 
     @After
@@ -54,15 +58,14 @@ public class MyMapperTest {
     }
 
     @Test
-    public void testMyMapperWithoutErrors() {
-        try {
-            List<Pair<Text, MyIntermediateWritable>> expectedOutput = new LinkedList<>();
-            expectedOutput.add(new Pair<>(new Text("ip1"), new MyIntermediateWritable(1L, 40028L)));
-            expectedOutput.add(new Pair<>(new Text("ip1"), new MyIntermediateWritable(1L, 56928L)));
-            expectedOutput.add(new Pair<>(new Text("ip2"), new MyIntermediateWritable(1L, 318L)));
-            expectedOutput.add(new Pair<>(new Text("ip3"), new MyIntermediateWritable(1L, 72209L)));
+    public void testMyMapReduceWithoutErrors() {
+        List<Pair<Text, MyFinalWritable>> expectedOutput = new LinkedList<>();
+        expectedOutput.add(new Pair<>(new Text("ip1"), new MyFinalWritable(96956L, 48478L)));
+        expectedOutput.add(new Pair<>(new Text("ip2"), new MyFinalWritable(318L, 318L)));
+        expectedOutput.add(new Pair<>(new Text("ip3"), new MyFinalWritable(72209L, 72209L)));
 
-            mapDriver
+        try {
+            mapReduceDriver
                     .withInput(new LongWritable(0L), new Text("ip1 - - [24/Apr/2011:04:06:01 -0400] \"GET /~strabal/grease/photo9/927-3.jpg HTTP/1.1\" 200 40028 \"-\" \"Mozilla/5.0 (compatible; YandexImages/3.0; +http://yandex.com/bots)\""))
                     .withInput(new LongWritable(1L), new Text("ip1 - - [24/Apr/2011:04:10:19 -0400] \"GET /~strabal/grease/photo1/97-13.jpg HTTP/1.1\" 200 56928 \"-\" \"Mozilla/5.0 (compatible; YandexImages/3.0; +http://yandex.com/bots)\""))
                     .withInput(new LongWritable(2L), new Text("ip2 - - [24/Apr/2011:04:20:20 -0400] \"GET /favicon.ico HTTP/1.1\" 200 318 \"-\" \"Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.16) Gecko/20110319 Firefox/3.6.16\""))
@@ -75,29 +78,29 @@ public class MyMapperTest {
         } catch (IOException ex) {
             fail("IOException should not be thrown");
         }
+
     }
 
     @Test
-    public void testMyMapperWithErrors() {
-        try {
-            List<Pair<Text, MyIntermediateWritable>> expectedOutput = new LinkedList<>();
-            expectedOutput.add(new Pair<>(new Text("ip1"), new MyIntermediateWritable(1L, 40028L)));
-            expectedOutput.add(new Pair<>(new Text("ip2"), new MyIntermediateWritable(1L, 318L)));
-            expectedOutput.add(new Pair<>(new Text("ip3"), new MyIntermediateWritable(1L, 72209L)));
+    public void testMyMapReduceWithErrors() {
+        List<Pair<Text, MyFinalWritable>> expectedOutput = new LinkedList<>();
+        expectedOutput.add(new Pair<>(new Text("ip1"), new MyFinalWritable(96956L, 48478L)));
+        expectedOutput.add(new Pair<>(new Text("ip2"), new MyFinalWritable(318L, 318L)));
 
-            mapDriver
+        try {
+            mapReduceDriver
                     .withInput(new LongWritable(0L), new Text("ip1 - - [24/Apr/2011:04:06:01 -0400] \"GET /~strabal/grease/photo9/927-3.jpg HTTP/1.1\" 200 40028 \"-\" \"Mozilla/5.0 (compatible; YandexImages/3.0; +http://yandex.com/bots)\""))
-                    .withInput(new LongWritable(1L), new Text("idp1 - - [24/Apr/2011:04:10:19 -0400] \"GET /~strabal/grease/photo1/97-13.jpg HTTP/1.1\" 200 56928 \"-\" \"Mozilla/5.0 (compatible; YandexImages/3.0; +http://yandex.com/bots)\""))
+                    .withInput(new LongWritable(1L), new Text("ip1 - - [24/Apr/2011:04:10:19 -0400] \"GET /~strabal/grease/photo1/97-13.jpg HTTP/1.1\" 200 56928 \"-\" \"Mozilla/5.0 (compatible; YandexImages/3.0; +http://yandex.com/bots)\""))
                     .withInput(new LongWritable(2L), new Text("ip2 - - [24/Apr/2011:04:20:20 -0400] \"GET /favicon.ico HTTP/1.1\" 200 318 \"-\" \"Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.16) Gecko/20110319 Firefox/3.6.16\""))
-                    .withInput(new LongWritable(3L), new Text("ip3 - - [24/Apr/2011:04:22:45 -0400] \"GET /personal/vanagon_1.jpg HTTP/1.1\" 200 72209 \"http://www.inetgiant.in/addetails/1985-vw-vanagon-transporter-single-cab-sinka-diesel-truck-2wd-rhd/3235819\" \"Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; WOW64; Trident/4.0; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET4.0C)\""))
+                    .withInput(new LongWritable(3L), new Text("i1p3 - - [24/Apr/2011:04:22:45 -0400] \"GET /personal/vanagon_1.jpg HTTP/1.1\" 200 72209 \"http://www.inetgiant.in/addetails/1985-vw-vanagon-transporter-single-cab-sinka-diesel-truck-2wd-rhd/3235819\" \"Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; WOW64; Trident/4.0; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET4.0C)\""))
                     .withAllOutput(expectedOutput)
-                    .withCounter(Browsers.FIREFOX, 1)
-                    .withCounter(Browsers.MSIE, 1)
                     .withCounter(Errors.ERRORS, 1)
+                    .withCounter(Browsers.FIREFOX, 1)
                     .runTest();
         } catch (IOException ex) {
             fail("IOException should not be thrown");
         }
+
     }
 
 }
