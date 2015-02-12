e5ddc43902518a1dc9cfc2bc767836e522c3f9a7
Oleksandr_Taranovsky <Oleksandr_Taranovsky@EPUAKYIW0052.kyiv.epam.com>
2015-02-12 11:44:44 +0200

    v0009

--- src/main/java/com/epam/training/hadoop/homework1/mapreduce/Runner.java
+++ src/main/java/com/epam/training/hadoop/homework1/mapreduce/Runner.java
@@ -21,11 +21,25 @@ import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
  */
 public class Runner {
 
+    private static final String INPUT_PATH_CONFIG = "mapreduce.homework1.inputpath";
+    private static final String OUTPUT_PATH_CONFIG = "mapreduce.homework1.outputpath";
+    
+    
     public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
 
         JobConf conf = new JobConf();
 
         conf.set("mapred.output.compression.codec", "org.apache.hadoop.io.compress.SnappyCodec");
+        
+        String inputPath = conf.get(INPUT_PATH_CONFIG);
+        String outputPath = conf.get(OUTPUT_PATH_CONFIG);
+        
+        if (inputPath == null) {
+            throw new RuntimeException("no input path set, try to set it by adding -D" + INPUT_PATH_CONFIG + "=<path>");
+        }
+        if (outputPath == null) {
+            throw new RuntimeException("no output path set, try to set it by adding -D" + OUTPUT_PATH_CONFIG + "=<path>");
+        }
         
         Job job = Job.getInstance(conf, "Log analyse");
 
@@ -42,8 +56,8 @@ public class Runner {
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputFormatClass(SequenceFileOutputFormat.class);
         
-        TextInputFormat.addInputPath(job, new Path(""));
-        SequenceFileOutputFormat.setOutputPath(job, new Path(""));
+        TextInputFormat.addInputPath(job, new Path(inputPath));
+        SequenceFileOutputFormat.setOutputPath(job, new Path(outputPath));
         
         SequenceFileOutputFormat.setOutputCompressionType(job, CompressionType.BLOCK);
         SequenceFileOutputFormat.setCompressOutput(job, true);
